912b9ce6e678d8eee8a8800c32f9d71de46aa1b0
numatanumanuma <chiat18kapi@icloud.com>
2020-12-01 15:04:57 +0900

    fixed bug

--- locations.py
+++ locations.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pyproj import Transformer
+import math
 
 class Locations:
 
@@ -12,7 +13,8 @@ class Locations:
         self.rect_epsg_ = 6677 # 平面直角座標系 9系
         tr = Transformer.from_proj(self.wgs84_espg_, self.rect_epsg_)
         for lat, lon in zip(df['Lat'], df['Lon']):
-            x, y = tr.transform(lat, lon)
-            self.data.append([lat, lon])
-            self.x_list.append(x)
-            self.y_list.append(y)
+            if not math.isnan(lat) and not math.isnan(lon):
+                x, y = tr.transform(lat, lon)
+                self.data.append([lat, lon])
+                self.x_list.append(x)
+                self.y_list.append(y)
--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from locations import Locations
 from plot_map import Map
 from plot_graph import Graph
 
-gnss = Locations('csv/sample.csv')
+single = Locations('csv/tableview_201201_single.csv')
+kinematic = Locations('csv/tableview_201201_ntrip.csv')
 
 start_location = [36.1086177, 140.0993047]
 
 pmap = Map(start_location, 800, 800)
-pmap.draw(gnss.data, 'red', 'gnss')
+pmap.draw(single.data, 'red', 'single')
+pmap.draw(kinematic.data, 'blue', 'kinematic')
 pmap.save()
 
 pgraph = Graph()
-pgraph.draw(gnss.x_list, gnss.y_list, 'red', 'gnss')
+pgraph.draw(single.x_list, single.y_list, 'red', 'single')
+pgraph.draw(kinematic.x_list, kinematic.y_list, 'blue', 'kinematic')
 pgraph.save()
